b7ef12dcfa8e838a082d178d195e9575fa53099b
Pedro Magno Müller <pm.mueller@pragmaticindustries.de>
2022-05-27 12:48:47 +0200

    part 2 basket

--- core/settings.py
+++ core/settings.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store',
+    'basket',
 ]
 
 MIDDLEWARE = [
@@ -44,6 +45,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'store.context_processors.categories',
+                'basket.context_processors.basket'
             ],
         },
     },
